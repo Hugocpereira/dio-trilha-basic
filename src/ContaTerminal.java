c30d29b5dfa8034c9b7ae0b616efc1895f34295e
Hugocpereira <hugocp100@hotmail.com>
2024-08-19 16:09:22 -0300

    teste

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,6 +1,27 @@
+import java.util.Scanner;
 
 public class ContaTerminal {
     public static void main(String[] args) {
-        System.out.println("Teste");
+        Scanner sc = new Scanner(System.in);
+
+        String nomeCliente;
+        double saldo;
+        int numeroConta;
+        String agencia;
+
+        System.out.println("Sistema Bancáio Teste Dio");
+        System.out.println("----------------------");
+        System.out.println("Digite o nome do cliente: ");
+        nomeCliente = sc.next();
+        System.out.println("Número da Agência: ");
+        agencia = sc.next();
+        System.out.println("Número da Conta: ");
+        numeroConta = sc.nextInt();
+        System.out.println("Saldo: ");
+        saldo = sc.nextDouble();
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar conta em nosso banco, sua agência é " + agencia +
+                ", conta " + numeroConta + " e seu saldo de R$ " + saldo + " já está disponível para saque.");
+
     }
 }
